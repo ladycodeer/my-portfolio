98707dec6d3c097502d3aa2af0ed99f2eb2e3f45
Rabia Kahya <rabiakahya22@gmail.com>
2023-03-30 22:16:39 +0300

    some changes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './about.css'
+import Rabia from '../../assets/Rabia.png'
 
 const about = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get to Know</h5>
+      <h2>About me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={Rabia} alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
