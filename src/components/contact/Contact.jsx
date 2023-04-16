42dfdde224bf7970bc4115100a1e6e3bcbfcfbfc
Rabia Kahya <rabiakahya22@gmail.com>
2023-04-17 01:28:36 +0300

    added emailjs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,9 +3,24 @@ import './contact.css'
 import { MdOutlineEmail } from 'react-icons/md'
 import { BsLinkedin } from 'react-icons/bs'
 import { BsGithub } from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 
-function Contact() {
+const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_zracuid', 'template_1be98ms', form.current, 'CGja3LrtMMDzbph3g')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+    e.target.reset()
+  };
+
   return (
     <section id='contact'>
 
@@ -23,14 +38,14 @@ function Contact() {
             <BsLinkedin className='contact__option-icon' />
             <h4>Linkedin</h4>
             {/* <h5>Reach out to me through linkedin</h5> */}
-            <a href="https://github.com/ladycodeer" target='_blank'> <h5>Reach out to me through linkedin</h5></a>
+            <a href="https://github.com/ladycodeer" target='_blank'> <h5>Reach out to me through LinkedIn</h5></a>
           </article>
 
 
 
         </div>
         {/* {End of contact Options} */}
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full Name' required />
           <input type="text" name='email' placeholder='Your Email' required />
           <textarea name="message" id="" rows="7" placeholder='Your Message' required></textarea>
